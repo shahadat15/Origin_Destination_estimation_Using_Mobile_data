b9d93b69009f4b64651c1c0eb07ad9e876293005
shahadat15 <miqba005@fiu.edu>
2016-04-25 01:28:41 -0400

    OD_estimation_file

--- OD_estimation.m
+++ OD_estimation.m
@@ -1,56 +1,75 @@
+%_____________Read the Data_________________%
+
 %Open the file
 fid=fopen('july15_order.csv');
 
-a=123456789;
-z=[];
-c=0;
-i=0;
-%k=0;
-%for k=1:k+1:1000,
- %tline = fgetl(fid);
-%end;
- %      a=tline(1:24);
-for j=1:j+1:5000000,
+ %this peart is to replace the string user ID with a real number
+userIDStr=123456789;
+userID_vector=[];
+userID=0;
+'''
+%For text purposes
+k=0;
+for k=1:k+1:1000,
+ tline = fgetl(fid);
+end;
+       userIDStr=tline(1:24);
+ 
+ '''
+
+for i=1:i+1:5000000,
     tline = fgetl(fid);
-    i=i+1;
-    if strfind(tline,a)>0
-        z(i,1)=c;
+    if strfind(tline,userIDStr)>0
+        userID_vector(i,1)=userID;
     else
-         c=c+1;
-        z(i,1)=c;
-        a=tline(1:24);
+         userID=userID+1;
+        userID_vector(i,1)=userID;
+        userIDStr=tline(1:24);
     end
+	i=i+1;
 end;
 fclose(fid);
 
+%read the other columns data 
+data_1=dlmread('july15_order.csv',',',[0,2,4999999,5]);
+%combine the data
+data_full=[userID_vector(:,1) data_1(:,1:4)];
 
-b=dlmread('july15_order.csv',',',[0,2,4999999,5]);
-m=[z(:,1) b(:,1:4)];
+clear userID_vector;
+clear data_1;
 
-clear z;
-clear b;
 
-h=zeros(33,33);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+%_____________Data Process_________________%
+
+%initialize
+ODflow=zeros(33,33);
 j=1;
-p=[];
-q=[];
-f=0;
-g=0;
-p(1,:)= m(1,2:5);
-for i=2:length(m),
-if m(i,1)== m(i-1,1)
+OneUser=[];
+OneUserSort=[];
+origin=0;
+destination=0;
+OneUser(1,:)= data_full(1,2:5);
+
+%separate userwise data
+for i=2:length(data_full),
+if data_full(i,1)== data_full(i-1,1)
     j=j+1; 
-    p(j,:)= m(i,2:5);
-    ag1=size(p);
-elseif ((ag1(1))>2)
-q = sortrows(p);
-ag=size(q);
-    %to eliminate second and third node
+    OneUser(j,:)= data_full(i,2:5);
+    size_1=size(OneUser);
+elseif ((size_1(1))>2)
+OneUserSort = sortrows(OneUser);
+size_2=size(OneUserSort);
+
+
+%eliminate second and third node
     l=1;
     r=[];
-for k=1:((ag(1,1))-1),
-    if  q(k,3)== 0
-        r(l,:)= q(k,2);
+for k=1:((size_2(1,1))-1),
+    if  OneUserSort(k,3)== 0
+        r(l,:)= OneUserSort(k,2);
         l=l+1;
     else
     end
@@ -58,46 +77,46 @@ end;
 o= unique(r);
 clear r;
 if length(o)> 1;
-for k=1:((ag(1,1))-1),
-    if q(k,3)> 0 && q(k,4)> 0
-        state = q(k,2);
+for k=1:((size_2(1,1))-1),
+    if OneUserSort(k,3)> 0 && OneUserSort(k,4)> 0
+        state = OneUserSort(k,2);
         switch state
-            case q(k,2)== o(1,1)
-                q(k,2)== o(1,1);
-            case q(k,3)== o(1,1)
-                q(k,2)== o(1,1);
-            case q(k,4)== o(1,1)
-                q(k,2)== o(1,1);
+            case OneUserSort(k,2)== o(1,1)
+                OneUserSort(k,2)== o(1,1);
+            case OneUserSort(k,3)== o(1,1)
+                OneUserSort(k,2)== o(1,1);
+            case OneUserSort(k,4)== o(1,1)
+                OneUserSort(k,2)== o(1,1);
         end
-    elseif (q(k,3)> 0 && q(k,4)== 0)
-        state = q(k,2);
+    elseif (OneUserSort(k,3)> 0 && OneUserSort(k,4)== 0)
+        state = OneUserSort(k,2);
         switch state
-            case q(k,2)== o(1,1)
-                q(k,2)== o(1,1);
-            case q(k,3)== o(1,1)
-                q(k,2)== o(1,1);
+            case OneUserSort(k,2)== o(1,1)
+                OneUserSort(k,2)== o(1,1);
+            case OneUserSort(k,3)== o(1,1)
+                OneUserSort(k,2)== o(1,1);
         end
     end;
 end;
 else
     clear o;
 end;
-    %end of node elimination
+%end of node elimination
     
          
-        for k=1:((ag(1,1))-1), 
-            if q(k,2)== q(k+1,2)
+        for k=1:((size_2(1,1))-1), 
+            if OneUserSort(k,2)== OneUserSort(k+1,2)
             else 
-                f = q(k,2);
-                g = q(k+1,2);
-                h(f,g) = (h(f,g)+1);
+                origin = OneUserSort(k,2);
+                destination = OneUserSort(k+1,2);
+                ODflow(origin,destination) = (ODflow(origin,destination)+1);
             end;
         end;
-        p=[];
-        q=[];
+        OneUser=[];
+        OneUserSort=[];
         j=1;
-        p(1,:)= m(i,2:5);
+        OneUser(1,:)= data_full(i,2:5);
 end;    
 end;
 %write the file
-xlswrite('ODflow',h)
+xlswrite('ODflow',ODflow)
